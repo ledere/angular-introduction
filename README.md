e5f658b483e49c7d19021e4343e6fc37fa521bf6
ledere <frank@duckbite.nl>
2014-12-19 14:40:33 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Angular introduction
+
+This is the supporting material / source code for the Angular Introduction course at Capgemini
+
+Features
+- grunt server
+- file watchers
+- unit test using karma
+- less compilation
+- build script
---
+++ src/app/app.spec.js
@@ -0,0 +1,19 @@
+describe('AppController', function () {
+    describe('isCurrentUrl', function () {
+//        var AppCtrl, $location, $scope;
+//
+//        beforeEach(module('angular-introduction'));
+//
+//        beforeEach(inject(function ($controller, _$location_, $rootScope) {
+//            $location = _$location_;
+//            $scope = $rootScope.$new();
+//            AppCtrl = $controller('AppController', { $location: $location, $scope: $scope });
+//        }));
+
+        it('should pass a dummy test', inject(function () {
+//            expect(AppCtrl).toBeTruthy();
+            expect(true).toBeTruthy();
+        }));
+    });
+});
+
